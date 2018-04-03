baa53cc4d876c6326592aebe32caaf592c43666c
luber <tomas.lubertino@gmail.com>
2018-04-03 17:27:16 -0300

    first implementation of initialize processor

--- src/data_processor.clj
+++ src/data_processor.clj
@@ -1,10 +1,42 @@
 (ns data-processor)
 
+(def rules '((define-counter "email-count" [] true)
+             (define-counter "spam-count" [] (current "spam"))
+             (define-signal {"spam-fraction" (/ (counter-value "spam-count" []) (counter-value "email-count" []))} true)
+             (define-counter "spam-important-table" [(current "spam") 
+                                                     (current "important")] true)))
+
 (defn initialize-processor [rules]
-  nil)
+  (for [lst rules]
+      (if (= "define-counter" (str (first lst)))
+          (define-rule-counter (rest lst))
+          (if (= "define-signal" (str (first lst)))
+            (define-rule-signal (rest lst))
+            (println " ")
+          )
+      )
+  )
+)
+
+(defn define-rule-counter [data]
+  (def name-counter (first data))
+  (def args (nth data 1))
+  (def condition (last data))
+  
+  (define-counter name-counter args condition)
+)
+
+(defn define-rule-signal [data]
+  (def signal (first data))
+  (def condition (last data))
+  
+  (define-signal signal condition)
+)
          
-(defn process-data [state new-data]
-  [nil []])
+(defn process-data [state new-data] [nil []]
+
+)
          
-(defn query-counter [state counter-name counter-args]
-  0)
+(defn query-counter [state counter-name counter-args] 0
+
+)
